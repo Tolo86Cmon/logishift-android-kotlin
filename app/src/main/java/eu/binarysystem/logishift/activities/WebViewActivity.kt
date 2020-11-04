f39649b6a7cd963a93dd6f1d427c91d0e2501b8c
Gualtiero Tolomeo <gualtiero.tolomeo@tualba.it>
2020-11-04 15:33:48 +0100

    configurato il progetto e le dipendenze

---
+++ app/src/main/java/eu/binarysystem/logishift/activities/WebViewActivity.kt
@@ -0,0 +1,15 @@
+package eu.binarysystem.logishift.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.binarysystem.logishift.R
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.webview_activity)
+
+        
+    }
+}
